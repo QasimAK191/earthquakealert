081d53df8d97bc890e688bc9366921f8e0be0f99
Qasim Khan <69918464+QasimAK191@users.noreply.github.com>
2023-09-20 20:25:44 -0400

    quake_list / App restructure

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,14 @@ import React, { Component } from 'react'
 class Header extends Component {
     render() {
         return (
+
             <header className="quakeAlert_header">
                 <h1 className="navbar__h1">QuakeAlert</h1>
                 <p className="navbar__p">Track earthquakes happening around the world, in real-time<br /></p>
             </header>
+            
+
+
         )
     }
 }
---
+++ src/components/QuakeListFilter.jsx
@@ -0,0 +1,159 @@
+import React, { Component } from 'react';
+import { Form, FormGroup, CustomInput, Input } from 'reactstrap';
+
+class QuakeListFilter extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            filterIsOpenAddDisplayClass: false,
+            magnitudeFilter: -1,
+            timeFilter: 0,
+            timeFormatFilter: 'gmt',
+            timeSort: 0,
+            magnitudeSort: 0
+        }
+
+        this.toggleFilter = this.toggleFilter.bind(this);
+        this.magnitudeChangeHandler = this.magnitudeChangeHandler.bind(this);
+        this.timeChangeHandler = this.timeChangeHandler.bind(this);
+        this.timeSortHandler = this.timeSortHandler.bind(this);
+        this.magnitudeSortHandler = this.magnitudeSortHandler.bind(this);
+    }
+
+    toggleFilter() {
+        this.setState({
+            filterIsOpenAddDisplayClass: !this.state.filterIsOpenAddDisplayClass
+        });
+    }
+
+    magnitudeChangeHandler(event) {
+        this.setState({
+            magnitudeFilter: event.target.value
+        });
+    }
+
+    timeChangeHandler(event) {
+        this.setState({
+            timeFilter: event.target.value
+        });
+    }
+
+    timeFormatChangeHandler(event) {
+        this.setState({
+            timeSort: event.target.value
+        });
+    }
+
+    magnitudeSortHandler(event) {
+        this.setState({
+            magnitudeSort: event.target.value
+        });
+    }
+
+    componentDidUpdate(prevProps, prevState) {   // This life cycle method is used to pass state data into parent component (QuakeList.js)
+        if (this.state.magnitudeFilter !== prevState.magnitudeFilter) {
+            // console.log("component update due to magnitude filter");
+            this.props.fetchFilterData(this.state);
+        } else if (this.state.timeFilter !== prevState.timeFilter) {
+            // console.log("component update due to time filter");
+            this.props.fetchFilterData(this.state);
+        } else if (this.state.timeFormatFilter !== prevState.timeFormatFilter) {
+            // console.log("component update due to time FORMAT filter");
+            this.props.fetchFilterData(this.state);
+        }
+    }
+
+    render() {
+
+        let filter_sec_class_list = ["filter_sec"];
+        if (!this.state.filterIsOpenAddDisplayClass) {
+            filter_sec_class_list.push('display_none');
+        }
+
+        // console.log("gives mag in string (got to convert it into Int)", this.state.magnitudeFilter);
+
+        return (
+            <>
+                <div className="toggle__filter" onClick={this.toggleFilter}>
+                    Filters
+                </div>
+                <div className={filter_sec_class_list.join(' ')}>
+                    <Form>
+                        <FormGroup >
+                            <CustomInput type="radio" id="magGreaterThan0" name="mag" value={-1} label="All" checked={parseInt(this.state.magnitudeFilter, 10) === -1} onChange={this.magnitudeChangeHandler} />
+                            <CustomInput type="radio" id="magGreaterThan3" name="mag" value={3.5} label="Magnitude &#8805; 3.5" checked={parseFloat(this.state.magnitudeFilter) === 3.5} onChange={this.magnitudeChangeHandler} />
+                            <CustomInput type="radio" id="magGreaterThan6" name="mag" value={6} label="Magnitude &#8805; 6" checked={parseInt(this.state.magnitudeFilter, 10) === 6} onChange={this.magnitudeChangeHandler} />
+                            <CustomInput type="radio" id="magGreaterThan8" name="mag" value={8} label="Magnitude &#8805; 8" checked={parseInt(this.state.magnitudeFilter, 10) === 8} onChange={this.magnitudeChangeHandler} />
+                        </FormGroup>
+                        <hr />
+                    </Form>
+                    <Form>
+                        <FormGroup>
+                            <CustomInput type="radio"
+                                id="lastmonth"
+                                name="timefilter"
+                                value={2591999999}
+                                label="Last 30 Days"
+                                checked={parseInt(this.state.timeFilter, 10) === 2591999999}
+                                onChange={this.timeChangeHandler} />
+                            <CustomInput type="radio"
+                                id="lastweek"
+                                name="timefilter"
+                                value={604800000}
+                                label="Last 7 Days"
+                                checked={parseInt(this.state.timeFilter, 10) === 604800000}
+                                onChange={this.timeChangeHandler} />
+                            <CustomInput type="radio"
+                                id="lastday"
+                                name="timefilter"
+                                value={86400000}
+                                label="Last 24 Hours"
+                                checked={parseInt(this.state.timeFilter, 10) === 86400000}
+                                onChange={this.timeChangeHandler} />
+                            <CustomInput type="radio"
+                                id="lasthour"
+                                name="timefilter"
+                                value={3600000}
+                                label="Last Hour"
+                                checked={parseInt(this.state.timeFilter, 10) === 3600000}
+                                onChange={this.timeChangeHandler} />
+                        </FormGroup>
+                        <hr />
+                    </Form>
+                    <Form>
+                        <FormGroup>
+                            <CustomInput type="radio" id="gmt" name="timeformatfilter" value="gmt" label="GMT Time Format" checked={this.state.timeFormatFilter === "gmt"} onChange={this.timeFormatChangeHandler} />
+                            <CustomInput type="radio" id="local" name="timeformatfilter" value="local" label="Local Time Format" checked={this.state.timeFormatFilter === "local"} onChange={this.timeFormatChangeHandler} />
+                            <hr />
+                            <p className="filter__sec__notice">*Defaults to 'Last 30 Hours'. <br />*Defaults to 'All' magnitude. <br />&#128308; Earthquakes with &#8805; 6 magnitude. <br /> &#128992; Earthquakes with &#8805; 4 and &#60; 6 magnitude. <br /> &#128309; Earthquakes with &#60; 4 magnitude.</p>
+                        </FormGroup>
+                    </Form>
+                    <Form>
+                        <FormGroup>
+                            <Label for="exampleSelect">Order by Time of Earthquake</Label>
+                            <Input type="select"
+                                id="recent"
+                                name="timeSortfilter"
+                                <option ///Checkbox or dropdown ;-;
+                                value={0}
+                                label="Most recent"
+                                checked={this.state.timeSort === { 0 }}
+                                onChange={this.timeSortChangeHandler} />
+                            <CustomInput type="radio"
+                                id="local"
+                                name="timeformatfilter"
+                                value="local"
+                                label="Local Time Format"
+                                checked={this.state.timeFormatFilter === "local"}
+                                onChange={this.timeFormatChangeHandler} />
+                        </FormGroup>
+                    </Form>
+                </div>
+            </>
+        )
+    }
+}
+
+export default QuakeListFilter;
