49a120f7e25e7648021bdff587f3b375a8c46711
Qasim Khan <69918464+QasimAK191@users.noreply.github.com>
2023-09-22 11:10:18 -0400

    Responsiveness #1 -Ahsan

--- src/components/Guide1.jsx
+++ src/components/Guide1.jsx
@@ -7,65 +7,60 @@ const Section = styled.div`
   height: 100vh;
   scroll-snap-align: center;
   display: flex;
-  flex-direction: column; /* Ensure items stack vertically on mobile */
+  flex-direction: column;
   align-items: center;
   justify-content: center;
 `;
 
 const Container = styled.div`
-  height: 100vh;
-  scroll-snap-align: center;
-  width: 1400px;
   display: flex;
+  flex-direction: column;
   justify-content: space-between;
+  
+  @media only screen and (min-width: 768px) {
+    flex-direction: row;
+  }
 `;
 
 const Left = styled.div`
   flex: 1;
-  position: relative;
   display: flex;
   padding-right: 20px;
-  justify-content: center; /* Horizontal centering */
-  align-items: center; /* Vertical centering */
-  height: 100vh
-  @media only screen and (max-width: 768px) {
-    flex: 0;
+  justify-content: center;
+  align-items: center;
+  
+  @media only screen and (max-width: 375px) {
     display: none;
   }
 `;
 
-//RIGHT SIDE
-
 const Right = styled.div`
   flex: 1;
   display: flex;
+
   flex-direction: column;
   justify-content: center;
+  align-items: center;
   gap: 20px;
-
-  @media only screen and (max-width: 768px) {
-    align-items: center;
-    text-align: center;
-    flex: 3;
+  @media only screen and (min-width: 380px) {
+    overflow-wrap: break-word; 
   }
 `;
 
 const Title = styled.h1`
-  font-size: 74px;
+  font-size: 32px;
 
-  @media only screen and (max-width: 768px) {
-    font-size: 60px;
-  }
-
-  @media only screen and (max-width: 667px) {
-    font-size: 40px;
+  @media only screen and (min-width: 768px) {
+    font-size: 74px;
   }
 `;
 
 const Subtitle = styled.h2`
   color: orange;
-  @media only screen and (max-width: 667px) {
-    font-size: 20px;
+  font-size: 20px;
+
+  @media only screen and (min-width: 768px) {
+    font-size: 40px;
   }
 `;
 
@@ -80,13 +75,12 @@ const Line = styled.img`
 `;
 
 const Desc = styled.p`
-  font-size: 24px;
+  font-size: 18px;
   color: Black;
 
-  @media only screen and (max-width: 768px) {
-    padding: 25px;
-    font-size: 15px;
-    overflow-wrap: break-word;
+  @media only screen and (min-width: 300px) {
+    font-size: 14px;
+    
   }
 `;
 
@@ -111,75 +105,65 @@ const Button = styled.div`
 `;
 
 const Img = styled.img`
-height: 30%;
-width: 100%;
-@media only screen and (max-width: 768px) {
-  display: none;
-}
+  max-width: 60%;
+  height: auto;
+  
+  @media only screen and (max-width: 375px) {
+    display: none;
+  }
 `;
 
 const linkStyle = {
-  color: "white",
-  textDecoration: "none",
+    color: "white",
+    textDecoration: "none",
 };
 
 const onButtonClick = () => {
-  // using Java Script method to get PDF file
-  fetch(PDFFile).then((response) => {
-    response.blob().then((blob) => {
-      // Creating new object of PDF file
-      const fileURL = window.URL.createObjectURL(blob);
-      // Setting various property values
-      let alink = document.createElement("a");
-      alink.href = fileURL;
-      alink.download = "Guide.pdf";
-      alink.click();
+    // using JavaScript method to get PDF file
+    fetch(PDFFile).then((response) => {
+        response.blob().then((blob) => {
+            // Creating a new object of the PDF file
+            const fileURL = window.URL.createObjectURL(blob);
+            // Setting various property values
+            let alink = document.createElement("a");
+            alink.href = fileURL;
+            alink.download = "Guide.pdf";
+            alink.click();
+        });
     });
-  });
 };
 
 class Guide1 extends Component {
-
     render() {
         return (
-
             <Section id="who">
-            <Container>
-              <Left>
-                <Img src="./img/drop_cover_hold.png"></Img>
-              </Left>
-              <Right>
-                <Title>Drop, Cover and Hold on</Title>
-                <WhatWeDo>
-                  <Line src="./img/line.png" />
-                  <Subtitle>When you feel the ground shake, immediately drop, cover and hold on.</Subtitle>
-                </WhatWeDo>
-                <ul>
-                        <li><b>Drop</b> to your hands and knees. If you’re inside, stay inside – don’t run outdoors or to other rooms
-                        </li>
-                        <li><b>Cover</b> your head and neck with your arm and take shelter under a sturdy piece of furniture.
-                
-                        </li>
-                        <li><b>Hold </b>
-                        on to your shelter, covering your head and neck until the shaking stops
-
-                        </li>
-                    </ul>
-                <Desc>
-                    
-                Prepare your home
-Before an earthquake, you can help prevent injuries and damage by making some changes inside your home. Severe shaking can topple large furniture and appliances, toss heavy items from walls and shelves, and throw open cupboards. 
-                </Desc>
-                <Button>
-                  <Link style={linkStyle} onClick={onButtonClick} smooth>
-                    Download Guide
-                  </Link>
-                </Button>
-              </Right>
-            </Container>
-          </Section>
-
-
+                <Container>
+                    <Left>
+                        <Img src="./img/drop_cover_hold.png" alt="Image" />
+                    </Left>
+                    <Right>
+                        <Title>Drop, Cover and Hold on</Title>
+                        <WhatWeDo>
+                            <Line src="./img/line.png" alt="Line" />
+                            <Subtitle>When you feel the ground shake, immediately drop, cover and hold on.</Subtitle>
+                        </WhatWeDo>
+                        <ul>
+                            <li><b>Drop</b> to your hands and knees. If you’re inside, stay inside – don’t run outdoors or to other rooms</li>
+                            <li><b>Cover</b> your head and neck with your arm and take shelter under a sturdy piece of furniture.</li>
+                            <li><b>Hold </b>on to your shelter, covering your head and neck until the shaking stops</li>
+                        </ul>
+                        <Desc>
+                            Prepare your home
+                            Before an earthquake, you can help prevent injuries and damage by making some changes inside your home. Severe shaking can topple large furniture and appliances, toss heavy items from walls and shelves, and throw open cupboards.
+            </Desc>
+                        <Button>
+                            <Link style={linkStyle} onClick={onButtonClick} smooth>
+                                Download Guide
+              </Link>
+                        </Button>
+                    </Right>
+                </Container>
+            </Section>
         )
     }
 }
