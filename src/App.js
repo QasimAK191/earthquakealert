678918e7e97f17e40b51dbbe3db5c355a78ab128
Qasim Khan <mohammedqasim.khan@ryerson.ca>
2023-09-22 09:41:35 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Header from "./components/Header.jsx";
 import Guide from "./Guide.js";
 import QuakeTracker from "./QuakeTracker.js";
 import Home from "./Home.js";
+import QuakeList from './components/QuakeList'
 
 {
   /*<!---------------------------- Start of  DONT TOUCH!!!!!!!!!!!!!!!!!!!!!!!!!!!!! -------------------------------------------------------------------------------------->*/
@@ -19,6 +20,7 @@ class App extends Component {
             <Route path="/">
               <Route index element={<Home />} />
               <Route path="/quake-tracker" element={<QuakeTracker />} />
+              <Route path="/quake-index" element={<QuakeList />} />
               <Route path="/quake-guide" element={<Guide />} />
             </Route>
           </Routes>
